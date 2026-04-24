21a6580c5098fad68230239b48bace8c4327e686
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2026-04-24 21:21:26 +0330

    document error hierarchy pt2

--- tests/test_core.py
+++ tests/test_core.py
@@ -116,3 +116,5 @@ def test_session_states_snapshotted_per_turn():
 
 def test_trace_states_survives_invalid():
     states = list(trace_states([
+        ConversationEvent.USER_MESSAGE,
+        ConversationEvent.TASK_DONE,
